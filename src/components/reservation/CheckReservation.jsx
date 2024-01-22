8b4110e3d3edc2f5ede792faf6cb1fb7ac54198b
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-01-22 20:21:48 +0600

    Minor changes

--- src/components/reservation/CheckReservation.jsx
+++ src/components/reservation/CheckReservation.jsx
@@ -3,14 +3,15 @@ const CheckReservation = ({reservedBookings}) => {
     console.log(reservedBookings)
     return (
         <div className="my-6 w-96 mx-auto p-8 bg-olive-50-transparent text-center">
-            <h2 className="text-center">{reservedBookings[0]?.name}, you have reservations on</h2>
-             { reservedBookings.length != 0 ?
+            <h2 className="text-center">{reservedBookings[0]?.name}</h2>
+           {reservedBookings.length > 0 && <span> You have reservations on
+                </span>} { reservedBookings.length != 0 ?
                 reservedBookings.map(each => (<p key={each?._id}>
                 {
                    each?.date
                 } <span className="ml-1">for</span> {
                     each?.people 
-                } <span className="mx-1">guests</span>
+                } <span className="mx-1">guests.</span>
                 {
                     <span>({each?.service})</span>
                 }
