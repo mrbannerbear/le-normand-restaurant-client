c257e6f6762e09c314f2e0d8ab7403e2e95e8f77
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-01-22 19:38:54 +0600

    Added reservation checker

--- src/components/reservation/CheckReservation.jsx
+++ src/components/reservation/CheckReservation.jsx
@@ -2,13 +2,19 @@
 const CheckReservation = ({reservedBookings}) => {
     console.log(reservedBookings)
     return (
-        <div className="my-6 w-96 mx-auto p-8 bg-olive-50-transparent">
-            { reservedBookings.length != 0 ?
-                reservedBookings.map(each => (<>
+        <div className="my-6 w-96 mx-auto p-8 bg-olive-50-transparent text-center">
+            <h2 className="text-center">{reservedBookings[0]?.name}, you have reservations on</h2>
+             { reservedBookings.length != 0 ?
+                reservedBookings.map(each => (<p key={each?._id}>
                 {
-                    each.name
+                   each?.date
+                } <span className="ml-1">for</span> {
+                    each?.people 
+                } <span className="mx-1">guests</span>
+                {
+                    <span>({each?.service})</span>
                 }
-                </>))
+                </p>))
                 :
                 <span className="text-center w-full">You have no reservations currently.</span>
             }
